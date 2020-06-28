cddf38d6e283899c5a45219495410fe5335485de
sorna tamanna <sornaewu@gmail.com>
2020-06-29 01:46:22 +0600

    Add project files.

---
+++ MVCAuthentication/Controllers/EmployeesController.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MVCAuthentication.Models;
+
+namespace MVCAuthentication.Controllers
+{
+    [Authorize]
+    public class EmployeesController : Controller
+    {
+        private OfficeEntities1 db = new OfficeEntities1();
+
+        
+        // GET: Employees
+        public ActionResult Index()
+        {
+            return View(db.Employees.ToList());
+        }
+        [Authorize(Roles ="Admin,Customer")]
+        // GET: Employees/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        // GET: Employees/Create
+
+        [Authorize(Roles ="Admin,Customer")]
+        
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Employees/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Admin,Customer")]
+        public ActionResult Create([Bind(Include = "Id,Name,Designation,Salary")] Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Employees.Add(employee);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(employee);
+        }
+
+        // GET: Employees/Edit/5
+        [Authorize(Roles = "Admin")]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        // POST: Employees/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Admin")]
+        public ActionResult Edit([Bind(Include = "Id,Name,Designation,Salary")] Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(employee).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(employee);
+        }
+
+        // GET: Employees/Delete/5
+        [Authorize(Roles = "Admin")]
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        // POST: Employees/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Admin")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Employee employee = db.Employees.Find(id);
+            db.Employees.Remove(employee);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [AllowAnonymous]
+        public ActionResult Abbout()
+        {
+            return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MVCAuthentication/Models/AccountController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCAuthentication.Models;
+using System.Web.Security; //sealed class which is used for Formsauthentication
+using System.Data.Entity.Infrastructure;
+
+
+namespace MVCAuthentication.Models
+{
+    [AllowAnonymous]
+    public class AccountController : Controller
+    {
+        // Login
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Membership model)
+        {
+            using (var context = new OfficeEntities1())
+            {
+                bool isValid = context.User.Any(x => x.UserName == model.UserName && x.Password == model.Password);
+                if(isValid)
+                {
+                    FormsAuthentication.SetAuthCookie(model.UserName, false);//(name of the user,browser a save krbo na tai false diyechi)
+                    return RedirectToAction("Index", "Employees");
+                }
+                ModelState.AddModelError("", "Invalid Username or Password");
+                return View();
+            }
+            
+        }
+        // Sign UP
+        public ActionResult Signup()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Signup(User model)
+        {
+            using (var context = new OfficeEntities1())
+            {
+                context.User.Add(model);
+                context.SaveChanges();
+            }
+            return RedirectToAction("Login");
+        }
+
+        // Logout
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+        }
+    }
+}
